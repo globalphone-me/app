1e70e189c09dc15eac13713e356b177520a71541
yssf-io <hey@yssf.io>
2025-11-22 08:40:22 -0300

    added world scaffolding

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ["https://9a22060a67ce.ngrok-free.app"],
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+
+import { MiniKit } from "@worldcoin/minikit-js";
+
 export default function Home() {
+  console.log("hello");
+  console.log(MiniKit.isInstalled());
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1>x402 Phone</h1>
+    <div>
+      <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <h1>x402 Phone</h1>
+      </div>
     </div>
   );
 }
